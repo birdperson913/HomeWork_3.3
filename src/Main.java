7aa21fb953e460e3c7fcb758a8f8cb9ac06562ba
SBirdperson <s.birdperson@gmail.com>
2022-08-05 18:10:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+CreditPaymentService creditPaymentService =new CreditPaymentService();
+        System.out.println(creditPaymentService.calculate(12,1000000,9.99));
+        System.out.println(creditPaymentService.calculate(24,1000000,9.99));
+        System.out.println(creditPaymentService.calculate(36,1000000,9.99));
+    }
+}
