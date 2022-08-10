4e33136a117fc15372f2c1103f0893ccbf99a3a9
SBirdperson <s.birdperson@gmail.com>
2022-08-10 15:55:25 +0300

    ReformatCode

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
-    public int calculate(int day , int summ , double percent){
-        double monthlyPayment = summ*((percent/100/12)+((percent/100/12)/(((Math.pow((1+percent/100/12),day))-1))));
+    public int calculate(int day, int summ, double percent) {
+        double monthlyPayment = summ * ((percent / 100 / 12) + ((percent / 100 / 12) / (((Math.pow((1 + percent / 100 / 12), day)) - 1))));
         return (int) monthlyPayment;
     }
 }
